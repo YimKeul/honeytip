93ea9859eb2c4e608354e423951e3b0ae4c604f1
YimKeul <43426556+YimKeul@users.noreply.github.com>
2021-12-25 20:13:58 +0900

    4-3

--- page/MainPage.js
+++ page/MainPage.js
@@ -11,6 +11,7 @@ import data from "../data.json";
 import Card from "../components/Cards";
 import Loading from "../components/Loding";
 import {StatusBar} from 'expo-status-bar'
+import * as Location from 'expo-location'
 const main =
   "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fmain.png?alt=media&token=8e5eb78d-19ee-4359-9209-347d125b322c";
 export default function MainPage({navigation, route}) {
@@ -28,11 +29,32 @@ export default function MainPage({navigation, route}) {
 
 
     setTimeout(() => {
+      getLocation()
       setState(data.tip);
       setCateState(data.tip);
       setReady(false);
     }, 1000);
   }, []);
+
+
+  const getLocation = async () => {
+    //수많은 로직중에 에러가 발생하면
+    //해당 에러를 포착하여 로직을 멈추고,에러를 해결하기 위한 catch 영역 로직이 실행
+    try {
+      //자바스크립트 함수의 실행순서를 고정하기 위해 쓰는 async,await
+      await Location.requestForegroundPermissionsAsync();
+      const locationData= await Location.getCurrentPositionAsync();
+      // console.log(locationData)
+      console.log("testing")
+      console.log(locationData.coords.latitude)
+      console.log(locationData.coords.longitude)
+
+    } catch (error) {
+      //혹시나 위치를 못가져올 경우를 대비해서, 안내를 준비합니다
+      Alert.alert("위치를 찾을 수가 없습니다.", "앱을 껏다 켜볼까요?");
+    }
+  }
+
 
   const category = (cate) => {
     if (cate == "전체보기") {
