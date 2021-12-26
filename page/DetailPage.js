8c59bc838f0cdf9afde8d6e6324d56c63b89fdd7
YimKeul <43426556+YimKeul@users.noreply.github.com>
2021-12-26 21:00:17 +0900

    4-12

--- page/DetailPage.js
+++ page/DetailPage.js
@@ -11,8 +11,14 @@ import {
   SafeAreaView,
   Share,
   Linking,
+  Platform,
+  TimePickerAndroid
 } from "react-native";
 import { firebase_db } from "../firebaseConfig";
+import * as Application from 'expo-application'
+const isIOS = Platform.OS === 'ios'
+
+
 
 export default function DetailPage({ navigation, route }) {
     // const [tip, setTip] = useState({
@@ -60,6 +66,23 @@ export default function DetailPage({ navigation, route }) {
   const link = () => {
     Linking.openURL("https://spartacodingclub.kr");
   };
+
+
+  const like = async() =>{
+      let userUniqueID
+      if(isIOS){
+          let iosID = await Application.getIosIdForVendorAsync()
+          userUniqueID = iosID
+      }else{
+          userUniqueID = await Application.androidId
+      }
+
+      console.log(userUniqueID)
+      firebase_db.ref('/like/'+userUniqueID+'/'+ tip.idx).set(tip,function(error){
+        console.log(error)
+        Alert.alert("찜 완료!")
+    });
+  }
   return ready ? null : (
     // ScrollView에서의 flex 숫자는 의미가 없습니다. 정확히 보여지는 화면을 몇등분 하지 않고
     // 화면에 넣은 컨텐츠를 모두 보여주려 스크롤 기능이 존재하기 때문입니다.
@@ -71,7 +94,7 @@ export default function DetailPage({ navigation, route }) {
         <Text style={styles.title}>{tip.title}</Text>
         <Text style={styles.desc}>{tip.desc}</Text>
         <View style={styles.buttonGroup}>
-          <TouchableOpacity style={styles.button} onPress={() => popup()}>
+          <TouchableOpacity style={styles.button} onPress={() => like()}>
             <Text style={styles.buttonText}>팁 찜하기</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={() => share()}>
