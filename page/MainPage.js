b290b4f70fca73226ad18108ef3dee9dfb14d5db
YimKeul <43426556+YimKeul@users.noreply.github.com>
2021-12-27 17:26:04 +0900

    조건에 따라 시작 이미지 변경 설정

--- page/MainPage.js
+++ page/MainPage.js
@@ -8,87 +8,88 @@ import {
   ScrollView,
   Alert,
   LogBox,
-  Platform
+  Platform,
 } from "react-native";
 import data from "../data.json";
 import Card from "../components/Cards";
 import Loading from "../components/Loding";
-import {StatusBar} from 'expo-status-bar'
-import * as Location from 'expo-location'
+import { StatusBar } from "expo-status-bar";
+import * as Location from "expo-location";
 import axios from "axios";
-import {firebase_db} from "../firebaseConfig"
+import { firebase_db } from "../firebaseConfig";
 // const main =
 //   "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fmain.png?alt=media&token=8e5eb78d-19ee-4359-9209-347d125b322c";
-export default function MainPage({navigation, route}) {
+export default function MainPage({ navigation, route }) {
   //return 구문 밖에서는 슬래시 두개 방식으로 주석
-  LogBox.ignoreLogs(['Warning: ...'])
-  LogBox.ignoreLogs(['Setting a timer']);
+  LogBox.ignoreLogs(["Warning: ..."]);
+  LogBox.ignoreLogs(["Setting a timer"]);
   const [state, setState] = useState([]); //전체데이터
   const [cateState, setCateState] = useState([]);
   const [weather, setWeather] = useState({
-    temp : 0,
-    condition : ''
-  })
+    temp: 0,
+    condition: "",
+  });
   const [ready, setReady] = useState(true);
-  // useEffect(()=>{
-  //   navigation.setOptions({
-  //     title:'나만의 꿀팁'
-  //   })  
-	// 	//뒤의 1000 숫자는 1초를 뜻함
-  //   //1초 뒤에 실행되는 코드들이 담겨 있는 함수
-  //   setTimeout(()=>{
-  //       firebase_db.ref('/tip').once('value').then((snapshot) => {
-  //         console.log("파이어베이스에서 데이터 가져왔습니다!!")
-  //         let tip = snapshot.val();
-          
-  //         setState(tip)
-  //         setCateState(tip)
-  //         getLocation()
-  //         setReady(false)
-  //       });
-  //       // getLocation()
-  //       // setState(data.tip)
-  //       // setCateState(data.tip)
-  //       // setReady(false)
-  //   },1000)
- 
-    
-  // },[])
 
-  const random= () => {
-    let min = 0
-    let max = 9
-    const rn =  Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log(`랜덤 뽑기 수 : ${rn}`)
-    return rn
-  }
-  const [main, setMain] = useState()
-  useEffect(()=>{
-        //헤더의 타이틀 변경
-          navigation.setOptions({
-              title:'나만의 꿀팁'
-          })
-          firebase_db.ref('/tip').once('value').then((snapshot) => {
-            // console.log("파이어베이스에서 데이터 가져왔습니다!!")
-            let tip = snapshot.val();
-            // console.log(tip[random()].idx)  
-            setMain(tip[random()].image)
-            setState(tip)
-            setCateState(tip)
-            getLocation()
-            setReady(false)
-            console.log("save")
-          });
-          // setTimeout(()=>{
-          //     let tip = data.tip;
-          //     setState(tip)
-          //     setCateState(tip)
-          //     getLocation()
-          //     setReady(true)
-          // },1000)
+  const [main, setMain] = useState();
+  const random = () => {
+    let min = 0;
+    const rn = Math.floor(Math.random() * (state.length - min)) + min;
+
+    return rn;
+  };
+
   
-      },[])
+  const main_img_ = (cate) => {
+    let test = {}
+    if(cate==="전체보기"){
+      test = state.filter((d) => {
+        return d.category == cate;
+      })
+    }else{
+      test = state.filter((d) => {
+        return d.category == cate;
+      })
+    }
+  
+    return test[0].image
+  };
 
+  const what_is_cat = (weather) => {
+    let cate = "";
+    if (weather == "Snow") {
+      cate = "생활";
+    } else {
+      cate = "반려견";
+    }
+    // console.log(cate)
+    return cate;
+  };
+
+  useEffect(() => {
+    //헤더의 타이틀 변경
+    navigation.setOptions({
+      title: "나만의 꿀팁",
+    });
+    firebase_db
+      .ref("/tip")
+      .once("value")
+      .then((snapshot) => {
+        // console.log("파이어베이스에서 데이터 가져왔습니다!!")
+        let tip = snapshot.val();
+        setState(tip);
+         //메인화면 이미지 랜덤함수 적용
+        setCateState(tip);
+        getLocation();
+        setReady(false);
+        what_is_cat(weather.weather);
+       ;
+        // setMain(tip[random()].image);
+        setMain( main_img_(what_is_cat(weather.weather)));
+        console.log("save");
+        console.log(main_img_(what_is_cat(weather.weather)))
+      });
+  }, []);
 
   const getLocation = async () => {
     //수많은 로직중에 에러가 발생하면
@@ -96,27 +97,23 @@ export default function MainPage({navigation, route}) {
     try {
       //자바스크립트 함수의 실행순서를 고정하기 위해 쓰는 async,await
       await Location.requestForegroundPermissionsAsync();
-      const locationData= await Location.getCurrentPositionAsync();
-      const latitude = locationData.coords.latitude
-      const longitude = locationData.coords.longitude
-      const  API_KEY = '62a3e1c15a974cb7212dd251dddb4fa7'
+      const locationData = await Location.getCurrentPositionAsync();
+      const latitude = locationData.coords.latitude;
+      const longitude = locationData.coords.longitude;
+      const API_KEY = "62a3e1c15a974cb7212dd251dddb4fa7";
       const result = await axios.get(
         `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
-      )
+      );
 
-      const weather = result.data.current.weather[0].main
-      const temp = result.data.current.temp
+      const weather = result.data.current.weather[0].main;
+      const temp = result.data.current.temp;
       // console.log(`날씨 : ${weather} , 온도 : ${temp}`)
-      setWeather({temp ,weather})
-
-
-        
+      setWeather({ temp, weather });
     } catch (error) {
       //혹시나 위치를 못가져올 경우를 대비해서, 안내를 준비합니다
       Alert.alert("위치를 찾을 수가 없습니다.", "앱을 껏다 켜볼까요?");
     }
-  }
-
+  };
 
   const category = (cate) => {
     if (cate == "전체보기") {
@@ -130,24 +127,21 @@ export default function MainPage({navigation, route}) {
       );
     }
   };
-  // let tip = data.tip; // useState 사용 전 이걸로 데이터 불러옴.
-  // let tip = state.tip
-  // let todayWeather = getLocation.weather;
-  // let todayCondition = getLocation.temp;
 
   return ready ? (
     <Loading />
   ) : (
-    /*
-      return 구문 안에서는 {슬래시 + * 방식으로 주석
-    */
-
     <ScrollView style={styles.container}>
-      <StatusBar style="light"/>
+      <StatusBar style="light" />
       <Text style={styles.weather}>
         오늘의 날씨: {weather.temp + "°C " + weather.weather}{" "}
       </Text>
-      <TouchableOpacity style = {styles.About_Btn} onPress={()=>navigation.navigate("AboutPage")}><Text style={{color:'white', fontWeight:'bold'}}>소개 페이지</Text></TouchableOpacity>
+      <TouchableOpacity
+        style={styles.About_Btn}
+        onPress={() => navigation.navigate("AboutPage")}
+      >
+        <Text style={{ color: "white", fontWeight: "bold" }}>소개 페이지</Text>
+      </TouchableOpacity>
       <Image style={styles.mainImage} source={{ uri: main }} />
       <ScrollView
         style={styles.middleContainer}
@@ -190,7 +184,7 @@ export default function MainPage({navigation, route}) {
           style={styles.middleButton04}
           onPress={() => {
             // category("꿀팁 찜");
-            navigation.navigate("LikePage")
+            navigation.navigate("LikePage");
           }}
         >
           <Text style={styles.middleButtonText}>꿀팁 찜</Text>
@@ -224,18 +218,16 @@ const styles = StyleSheet.create({
   weather: {
     alignSelf: "flex-end",
     paddingRight: 20,
-    paddingBottom : 10,
+    paddingBottom: 10,
   },
-  About_Btn:{
-    
-    backgroundColor : 'pink',
-    width : '30%',
-    alignItems :'center',
-    padding  : '2%',
-    borderRadius : 20,
-    alignSelf :'flex-end',
-    marginRight : 20
-    
+  About_Btn: {
+    backgroundColor: "pink",
+    width: "30%",
+    alignItems: "center",
+    padding: "2%",
+    borderRadius: 20,
+    alignSelf: "flex-end",
+    marginRight: 20,
   },
   mainImage: {
     //컨텐츠의 넓이 값
